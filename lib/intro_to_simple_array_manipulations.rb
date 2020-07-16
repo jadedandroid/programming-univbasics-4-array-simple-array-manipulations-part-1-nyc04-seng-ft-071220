3286662b1bf2259f651db79b4a86ab2d945b81c9
jade decordova <lacroixjaded@gmail.com>
2020-07-16 15:35:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,26 @@ def using_push(array, string)
 
 end
 
-def
+def using_unshift(array,string)
+  array.unshift(string)
 
+end
+
+def  using_pop(array)
+  array.pop
+
+end
+
+def pop_with_args(array, num)
+    array.pop(num)
+end
+
+def using_shift(array)
+  array.shift
+
+end
+
+
+def shift_with_args(array, num)
+    array.shift(num)
 end
